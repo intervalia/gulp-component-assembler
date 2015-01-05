143827099c443107c05189b267db49458b89a678
Mike Collins <intervalia@gmail.com>
2015-01-05 14:01:16 -0700

    Adding .coveralls.yml file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var Mocha = require('mocha');
+var compasm = require('./src/index');
 
 gulp.task('lint', function () {
   return gulp.src([
@@ -17,4 +18,10 @@ gulp.task('test', ['lint'], function (done) {
   var m = new Mocha();
   m.addFile(__dirname + '/test/specs/index.js');
   m.run().on('end', done);
+});
+
+gulp.task("build", function() {
+  return gulp.src(['testdata/**/assembly.json'])
+  .pipe(compasm.assemble())
+  .pipe(gulp.dest('testdata/prod'));
 });
